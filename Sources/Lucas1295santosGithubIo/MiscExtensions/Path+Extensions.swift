10775d1b0611f9d7ade5e89037ac7f4c2b215d12
lucas santos <lucas1295santos@gmail.com>
2020-04-17 22:35:36 -0300

    Fixes image extension

--- Sources/Lucas1295santosGithubIo/MiscExtensions/Path+Extensions.swift
+++ Sources/Lucas1295santosGithubIo/MiscExtensions/Path+Extensions.swift
@@ -2,11 +2,11 @@ import Publish
 
 extension Path {
     static func forImage(named name: String, withExtension imageExtension: ImageExtension) -> Path {
-        return Path("images/\(name)\(imageExtension.rawValue)")
+        return Path("../images/\(name)\(imageExtension.rawValue)")
     }
 }
 
 enum ImageExtension: String {
-    case jpg = ".JPG"
+    case jpg = ".jpg"
     case png = ".png"
 }
